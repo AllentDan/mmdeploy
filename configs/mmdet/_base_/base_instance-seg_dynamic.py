3942e2fb16f225639585cb44e2bf56ef64b7019b
dongchunyu.vendor <dongchunyu@pjlab.org.cn>
2022-06-22 15:53:41 +0800

    enable dynamic for trt

--- configs/mmdet/_base_/base_instance-seg_dynamic.py
+++ configs/mmdet/_base_/base_instance-seg_dynamic.py
@@ -6,18 +6,18 @@ onnx_config = dict(
             2: 'height',
             3: 'width'
         },
-        # 'dets': {
-        #     0: 'batch',
-        #     1: 'num_dets',
-        # },
-        # 'labels': {
-        #     0: 'batch',
-        #     1: 'num_dets',
-        # },
-        # 'masks': {
-        #     0: 'batch',
-        #     1: 'num_dets',
-        #     2: 'height',
-        #     3: 'width'
-        # },
+        'dets': {
+            0: 'batch',
+            1: 'num_dets',
+        },
+        'labels': {
+            0: 'batch',
+            1: 'num_dets',
+        },
+        'masks': {
+            0: 'batch',
+            1: 'num_dets',
+            2: 'height',
+            3: 'width'
+        },
     })
